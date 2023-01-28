e98a817510cb0a4a0a3e8cbef7706365728800f7
gurunzpritam <108646957+gurunzpritam@users.noreply.github.com>
2023-01-28 21:50:44 +0545

    Update server.py

--- FLASK_FINAL/server.py
+++ FLASK_FINAL/server.py
@@ -42,4 +42,6 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug = False)
+    app.run(host=f"0.0.0.0:{PORT}",debug=False)
+    #app.run(host="0.0.0.0",debug = False)
+    
